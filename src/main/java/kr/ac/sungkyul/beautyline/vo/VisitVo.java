e8eeeb21b0a68daed18117ef24d1fb1528f45e33
WonHo <WonHo@DESKTOP-AV039T4>
2016-09-08 12:25:25 +0900

    test wh

---
+++ src/main/java/kr/ac/sungkyul/beautyline/vo/VisitVo.java
@@ -0,0 +1,10 @@
+package kr.ac.sungkyul.beautyline.vo;
+
+public class VisitVo {
+	private long no;
+	private long userNo;
+	private long programNo;
+	private long iamgeNo;
+	private String memo;
+	private String regDate;
+}

f54f1e50553fca5a0816ba0ed6377666bfc16107
Hyobin-Im <hbpencil12@naver.com>
2016-10-11 16:43:23 +0900

    notiboard 좀 수정하고 커밋!

--- src/main/java/kr/ac/sungkyul/beautyline/controller/NoticeBoardController.java
+++ src/main/java/kr/ac/sungkyul/beautyline/controller/NoticeBoardController.java
@@ -38,11 +38,7 @@ public class NoticeBoardController {
    @RequestMapping("board")
    public String list(Model model, @RequestParam(value = "nowPage", required = false) Integer nowPage,
          @RequestParam(value = "nowBlock", required = false) Integer nowBlock
-   /*
-    * @RequestParam(value = "keyField", required=false) String keyField,
-    * 
-    * @RequestParam(value = "keyWord", required=false) String keyWord
-    */) {
+  ) {
       List<NoticeBoardVo> boardList = nBoardService.getAll();
       PageVo page = null;
       try {
@@ -53,10 +49,7 @@ public class NoticeBoardController {
       }
       model.addAttribute("boardList", boardList);
       model.addAttribute("page", page);
-      /*
-       * model.addAttribute("keyField", keyField);
-       * model.addAttribute("keyWord", keyWord);
-       */
+    
       return "board/noticeboard/board";
    }
 
@@ -181,89 +174,6 @@ public class NoticeBoardController {
       fin.close();
        
     }
-   
-   
-   
-   /*
-    * 이미지 업로드
-    * 
-    * @ResponseBody
-    * 
-    * @RequestMapping(value="/imaUpload", method = RequestMethod.POST, produces
-    * = "text/html; charset=UTF-8") public String
-    * imaUpload(@RequestParam("file") MultipartFile file)throws Exception{
-    * String orgName= file.getOriginalFilename(); // file.get
-    *  String originFileName = imafile.getOriginalFilename();
-    * 
-    * 
-    * return ""; }
-    */
 
-   /*
-    * @ResponseBody
-    * 
-    * @RequestMapping(value = "/imaUpload", method = RequestMethod.POST) public
-    * String
-    * imaUpload(MultipartHttpServletRequest req, HttpServletResponse res){
-    * 
-    *  Iterator<String> itr =  request.getFileNames();
-    *     MultipartFile mpf = request.getFile(itr.next());
-    *     String originFileName = mpf.getOriginalFilename();  return "";
-    * 
-    * }
-    */
-   /*
-    * public void insertBoard(BBSVo BBSVo, MultipartFile file) throws
-    * Exception{
-    * 
-    * 
-    * //1. fno --> 저장할때
-    * 
-    * //2. no --> 게시글 저장할때 Long no = bbsDao.insertBoard(BBSVo);
-    * System.out.println(no); //3. orgName String orgName
-    * =file.getOriginalFilename();
-    * 
-    * //4. fileSize long fileSize = file.getSize();
-    * 
-    * //5. saveName String saveName = UUID.randomUUID().toString()+"_"+orgName;
-    * 
-    * //6. path String path = "c:\\Users\\S401-11\\Downloads\\filestore";
-    * 
-    * 
-    * AttachFileVo attachFileVo = new AttachFileVo(); attachFileVo.setNo(no);
-    * attachFileVo.setPath(path); attachFileVo.setFileSize(fileSize);
-    * attachFileVo.setOrgName(orgName); attachFileVo.setSaveName(saveName);
-    * 
-    * bbsDao.insertAttachFile(attachFileVo);
-    * 
-    * File target = new File(path, saveName);
-    * FileCopyUtils.copy(file.getBytes(), target); }
-    */
-
-   //=======================================================
-      
-      /*    String originFileName = mpf.getOriginalFilename();
-            System.out.println(originFileName);
-            System.out.println(noticeBoardVo.toString());
-         System.out.println(request);
-         Iterator<String> itr =  request.getFileNames();
-          
-                  MultipartFile mpf = request.getFile(itr.next());
-                  System.out.println(mpf.getOriginalFilename() +" uploaded!");
-                  try {
-                      //just temporary save file info into ufile
-                      System.out.println("file length : " + mpf.getBytes().length);
-                      System.out.println("file name : " + mpf.getOriginalFilename());
-                  } catch (IOException e) {
-                      System.out.println(e.getMessage());
-                      e.printStackTrace();
-                  }
-              */
-         
-   /*      nBoardService.write(vo);
-         @RequestParam("file") MultipartFile file, @PathVariable("boardName") String boardName
-      
-   */
-   //=========================================================
    
 }
